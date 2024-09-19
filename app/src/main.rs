8983dbcb2382f449f048d2d6d0c9bcbd86af3e1d
David Binder <david.binder@uni-tuebingen.de>
2024-09-19 23:14:39 +0200

    Implement CLI interface

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,9 +1,29 @@
 use std::path::PathBuf;
 
+use clap::Parser;
 use tacky_lib::{assemble_and_link, compile, preprocess};
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct CLI {
+    /// Run the lexer, but stop before parsing.
+    #[arg(long)]
+    lex: bool,
+    /// Run the parser, but stop before assembly generation.
+    #[arg(long)]
+    parse: bool,
+    /// Generate assembly, but stop before code generation.
+    #[arg(long)]
+    codegen: bool,
+    filepath: PathBuf,
+}
 fn main() {
-    let mut example: PathBuf = PathBuf::from("examples/return_2.c");
+    let cli = CLI::parse();
+    exec(&cli)
+}
+
+fn exec(cli: &CLI) {
+    let mut example: PathBuf = cli.filepath.clone();
 
     preprocess(&example);
     example.set_extension("i");
